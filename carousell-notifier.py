3cb8737d415d1a4e403305bbddb3ed30a8fc9314
Shyam from Pi <sundar.vinod@gmail.com>
2017-09-06 17:32:23 +0800

    Managed to get script

--- carousell-notifier.py
+++ carousell-notifier.py
@@ -5,16 +5,24 @@ from bs4 import BeautifulSoup
 from pushbullet import Pushbullet
 
 import urllib
+import cfscrape
 
 # user provided info
 pushbulletAPI = "o.AHKh671q9bc91LVmvRx4olNE7eV3LIgF"
-carousellWebsiteLink = "https://carousell.com/"
+carousellWebsiteLink = "https://carousell.com/search/products/?query="
+searchTerm = "overcoming+gravity" # note: search term should not have spaces, replace with +
 
 # process carousell URL
 def processURL(link):
-	url = urllib.urlopen(link).read()
+	scraper = cfscrape.create_scraper()
+	url = scraper.get(carousellWebsiteLink + searchTerm).content
 	soup = BeautifulSoup(url, "lxml")
 
-	print(soup.prettify())
+	# print(soup.prettify().encode('utf-8'))
+	# for link in soup.find_all('script'):
+	# 	print(len(list(link.children)))
+	link = soup.find_all('script')
+	children = link[1].children
+	print(list(children)[0])
 
 processURL(carousellWebsiteLink)
