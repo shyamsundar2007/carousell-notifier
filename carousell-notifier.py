783535d9216053495cc286d719eb32c576b3cb35
Shyam from Pi <sundar.vinod@gmail.com>
2017-09-07 11:16:59 +0800

    First working version supporting multiple search terms

--- carousell-notifier.py
+++ carousell-notifier.py
@@ -6,11 +6,31 @@ from pushbullet import Pushbullet
 
 import urllib
 import cfscrape
+import pickle
+import re
 
 # user provided info
 pushbulletAPI = "o.AHKh671q9bc91LVmvRx4olNE7eV3LIgF"
 carousellWebsiteLink = "https://carousell.com/search/products/?query="
-searchTerm = "overcoming+gravity" # note: search term should not have spaces, replace with +
+searchTerms = [] # note: search term should not have spaces, replace with +
+
+# global vars - do not change!
+newListings = []
+
+# carousell object
+class Carousell(object):
+	def __init__(self):
+		self.id = 0		# (integer) id for carousell object
+		self.title = ""		# (string) title for listing
+		self.desc = ""		# (string) description for listing
+	def __hash__(self):
+		return hash(self.id)
+	def __eq__(self, other):
+		return (isinstance(other, self.__class__) and getattr(other, 'id') == self.id)
+	def addListing(self, listingId, listingTitle, listingDesc):
+		self.id = listingId;
+		self.title = listingTitle;
+		self.desc = listingDesc;	
 
 # process carousell URL
 def processURL(link):
@@ -22,7 +42,78 @@ def processURL(link):
 	# for link in soup.find_all('script'):
 	# 	print(len(list(link.children)))
 	link = soup.find_all('script')
-	children = link[1].children
-	print(list(children)[0])
+	content = link[1].contents[0]
+	#print(content)
+	unicodeContent = unicode(content)
+	pattern = re.compile("\"id\"\:(\d*)\,\"title\"\:\"(.*?)\"\,\"description\"\:\"(.*?)\"")
+	matches = re.findall(pattern, unicodeContent)
+	for matchObj in matches:
+		listing = Carousell();
+		listing.id = int(matchObj[0])
+		listing.title = unicode(matchObj[1])
+		listing.desc = unicode(matchObj[2])
+		newListings.append(listing)
 
-processURL(carousellWebsiteLink)
+# read search terms from file
+try: 
+	file = open("searchTerms.txt", 'r')
+	for line in file:
+		searchTerms.append(line)
+	file.close()
+except IOError:
+	print "File not found. Nothing to search for!"
+
+# fetch new listings for each search term
+for searchTerm in searchTerms:
+	print ("Searching for " + searchTerm.rstrip() + " on carousell website...")
+	# clear new fetchings for each search term
+	newListings = []
+
+	# fetch listings for search term
+	processURL(carousellWebsiteLink + searchTerm)
+
+	# compare new listings with old
+	oldListings = []
+	pb = Pushbullet(pushbulletAPI)
+	oldFileName = "oldListings" + searchTerm.rstrip() + ".pkl"
+
+	try:
+		with open(oldFileName, 'rb') as input:
+			while True:
+				try:
+					oldListing = pickle.load(input)
+					# print oldListing.title
+					oldListings.append(oldListing)
+				except (EOFError):
+					break
+	except IOError:
+		print "File not found. Continuing anyways...\n"
+
+	newListingsAdded = list(set(newListings) - set(oldListings))
+
+	'''
+	print "New listings in website: "
+	for listing in newListings:
+		print listing.title
+	print " "
+
+	print "Old listings stored internally: "
+	for listing in oldListings:
+		print listing.title
+	print " "
+	'''
+
+	#print "New listings to be added to storage: "
+	for listing in newListingsAdded: 
+		#print listing.title
+		 push = pb.push_note("A new listing has been found for " + searchTerm.rstrip(), listing.title + listing.desc)
+	#print " "
+
+	print ("There were " + str(len(newListings)) + " listings found on the website with " + str(len(newListingsAdded)) + " listings newly added")
+	print " "
+
+	# append new listings to old listings storage
+	for listing in newListingsAdded:
+		with open(oldFileName, 'ab') as output:
+		# 	print ("Adding listings: " + str(listing.id))
+			pickle.dump(listing, output, -1)
